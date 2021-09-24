caddd7c4837cab7b999e012f543ab30e8960a002
Doug Alderman <agnadcp-d1@yahoo.com>
2021-09-24 12:37:30 -0400

    Working on table displaying correctly.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,6 +12,26 @@ export class AppComponent {
 
   docs: any[] = [];
 
+  docSelection: FormGroup = this.fb.group({
+    filename: new FormControl(''),
+    print: new FormControl(''),
+    color: new FormControl(''),
+    notes: new FormControl(''),
+  })
+
+  docSelections: FormArray = this.fb.array([this.docSelection]);
+
+  printCenterOptions = this.fb.group({
+    deliverTo: new FormControl(''),
+    specialInstructions: new FormControl(''),
+    selections: this.docSelections
+  });
+
+
+  get selections() {
+    return this.printCenterOptions.get('selections') as FormArray;
+  }
+
   constructor(
     private fb: FormBuilder,
     private apiService: PrintCenterApiService
@@ -26,25 +46,29 @@ export class AppComponent {
           if (data && data.documents && data.documents.filename && data.documents.filename.length) {
             this.docs = data.documents.filename;
             console.log('this.docs: ', this.docs);
+            this.docSelections.setControl(0, 
+              this.fb.group({
+                filename: this.docs[0],
+                print: '',
+                color: '',
+                notes: ''
+              })  
+            );
+            for (let i = 1; i < this.docs.length; i++) {
+              this.docSelections.push(
+                this.fb.group({
+                  filename: this.docs[i],
+                  print: '',
+                  color: '',
+                  notes: ''
+                })  
+              );
+            }
+            console.log('this.docSelections.controls: ', this.docSelections.controls);
           }
-        }
-      )
+        }  
+      );
   }
-
-  docSelection: FormGroup = this.fb.group({
-    print: new FormControl(''),
-    color: new FormControl(''),
-    notes: new FormControl(''),
-  })
-
-  docSelections: FormArray = this.fb.array([this.docSelection]);
-
-  printCenterOptions = this.fb.group({
-    deliverTo: new FormControl(''),
-    specialInstructions: new FormControl(''),
-    selections: this.docSelections
-  });
-
 
   onSubmit(): void {
     console.log('submitted')
